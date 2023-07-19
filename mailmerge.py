946e9784845002db31efd120b1394526a28eccfd
Miguel Ángel Robledo <miguel.robledo@scanda.com.mx>
2023-07-19 05:09:10 -0600

    quit find merge field row

--- mailmerge.py
+++ mailmerge.py
@@ -322,12 +322,10 @@ class MailMerge(object):
                     logging.warning(row_data)
                     for idxx, row in enumerate(table_to_use):
                         rowIns = deepcopy(row)
-                        if row.find('.//MergeField[@name="%s"]' % list(row_data.keys())[0]) is not None:
-                            logging.warning('found data')
-                            self.merge([rowIns], **row_data)
+                        self.merge([rowIns], **row_data)
                         logging.warning('insert in table')
                         logging.warning(rowIns)
-                        table.insert(idxx + i, rowIns)
+                        table.insert(idx + idxx + i, rowIns)
             else:
                 # if there is no data for a given table
                 # we check whether table needs to be removed
